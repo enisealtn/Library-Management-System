4451fb9cf9959a4c6d24abf340a5e75f5b1407bf
enisealtn <67960749+enisealtn@users.noreply.github.com>
2023-05-21 02:33:04 +0300

    Update ArtBook.java

--- ArtBook.java
+++ ArtBook.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class ArtBook extends Book{
 	private String genre;
-	private boolean award;
+	private String award;
 	
 	public ArtBook() {
 		
+	}
+	
+	public ArtBook(int bookId, String bookName, String author, String location, String format, String isBorrowed, String dateBorrowed, String genre, String award) {
+		super(bookId, bookName, author, location, format, isBorrowed, dateBorrowed);
+		this.genre = genre;
+		this.award = award;
 	}
 	
 	public void getInput() {
@@ -14,28 +20,28 @@ public class ArtBook extends Book{
 		System.out.println("Enter the genre of the book = ");
 		genre = scan.nextLine();
 		System.out.println("Has this book any award (true/false)?");
-		award = scan.nextBoolean();
+		award = scan.nextLine();
 	}
 	
 	@Override
 	public void read() {
 		super.setFormat("Not Readable");
-		
 	}
+	
 	@Override
 	public void findLocation(String bookName) {
-		super.setLocation("ART-"+ bookName.toUpperCase().split("\\s"));		
+		super.setLocation("ART-"+ bookName.toUpperCase().replace(" ", ""));		
 	}
 	
 	public String toString() {
 		String str;
 		str = super.toString() + 
 				"\nGenre of the book = " + genre + 
-				"\nAward situation = ";
-		if (award)
+				"\nAward situation = " + award;
+		/*if (award)
 			str += "yes";
 		else
-			str += "no";
+			str += "no";*/
 		return str;
 	}
 }
