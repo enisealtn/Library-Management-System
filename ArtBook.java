68a178330ed1c44444c4a8546da2f730c5dbd4d0
NidaAktas <134498190+NidaAktas@users.noreply.github.com>
2023-05-27 13:19:11 +0300

    ArtBook.java

--- ArtBook.java
+++ ArtBook.java
@@ -1,47 +1,53 @@
 import java.util.Scanner;
 
-public class ArtBook extends Book{
-	private String genre;
-	private String award;
-	
-	public ArtBook() {
-		
-	}
-	
-	public ArtBook(int bookId, String bookName, String author, String location, String format, String isBorrowed, String dateBorrowed, String genre, String award) {
-		super(bookId, bookName, author, location, format, isBorrowed, dateBorrowed);
-		this.genre = genre;
-		this.award = award;
-	}
-	
-	public void getInput() {
-		Scanner scan = new Scanner(System.in);
-		super.getInput();
-		System.out.println("Enter the genre of the book = ");
-		genre = scan.nextLine();
-		System.out.println("Has this book any award (true/false)?");
-		award = scan.nextLine();
-	}
-	
-	@Override
-	public void read() {
-		super.setFormat("Not Readable");
-	}
-	
-	@Override
-	public void findLocation(String bookName) {
-		super.setLocation("ART-"+ bookName.toUpperCase().replace(" ", ""));		
-	}
-	
-	public String toString() {
-		String str;
-		str = super.toString() + 
-				"\nGenre of the book = " + genre + 
-				"\nAward situation = " + award;
-		/*if (award)
-			str += "yes";
-		else
-			str += "no";*/
-		return str;
-	}
+public class ArtBook extends Book {
+    private String genre;
+    private String award;
+
+    public ArtBook() {
+
+    }
+
+    public ArtBook(int bookId, String bookName, String author, String location, String format, String isBorrowed, String dateBorrowed, String genre, String award) {
+        super(bookId, bookName, author, location, format, isBorrowed, dateBorrowed);
+        this.genre = genre;
+        this.award = award;
+    }
+
+    @Override
+    public void getInput() {
+        Scanner scan = new Scanner(System.in);
+        super.getInput();
+        System.out.print("Enter the genre of the book: ");
+        genre = scan.nextLine();
+        System.out.print("Does this book have any awards (true/false)? ");
+        award = scan.nextLine();
+        System.out.print("Enter the format of the encyclopedia: ");
+        format = scan.nextLine();
+    }
+
+    @Override
+    public void read() {
+        if (format.equalsIgnoreCase("printed")) {
+        	super.setFormat("Readable");
+        } else if (format.equalsIgnoreCase("mp3")) {
+        	super.setFormat("Not Readable");
+        } else {
+        	super.setFormat("Invalid Format");
+        }
+    }
+
+    @Override
+    public void findLocation(String bookName) {
+        super.setLocation("ART-" + bookName.toUpperCase().replace(" ", ""));
+    }
+
+    @Override
+    public String toString() {
+        String readableStatus = super.getFormat().equals("Readable") ? "Readable" : "Not Readable";
+        return super.toString() +
+                "\nGenre of the book: " + genre +
+                "\nAward situation: " + award +
+                "\nReadability: " + readableStatus;
+    }
 }
