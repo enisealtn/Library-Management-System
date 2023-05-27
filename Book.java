40a601aa8a3668fba5920fb4159a90cd11813655
NidaAktas <134498190+NidaAktas@users.noreply.github.com>
2023-05-27 13:19:56 +0300

    Book.java

--- Book.java
+++ Book.java
@@ -1,63 +1,104 @@
 import java.util.Scanner;
 
-public abstract class Book implements Readable{
-	protected int bookId;
-	protected String bookName;
-	protected String author;
-	//protected String library; //What is the purpose of this variable?
-	protected String location;
-	protected String format;
-	protected String isBorrowed;
-	protected String dateBorrowed; //"mm/dd/yyyy"
-	public Book() {
-		
-	}
-	
-	public Book(int bookId, String bookName, String author, String location, String format, String isBorrowed, String dateBorrowed) {
-		this.bookId = bookId;
-		this.bookName = bookName;
-		this.author = author;
-		this.location = location;
-		this.format = format;
-		this.isBorrowed = isBorrowed;
-		this.dateBorrowed = dateBorrowed;
-	}
-	
-	//Getters and setters can be adjusted for the purpose
-	
-	public String getLocation() {
-		return location;}
-	
-	public String getFormat() {
-		return format;}
+public abstract class Book implements Readable {
+    protected int bookId;
+    protected String bookName;
+    protected String author;
+    protected String location;
+    protected String format;
+    protected String isBorrowed;
+    protected String dateBorrowed; //"mm/dd/yyyy"
 
-	public void setLocation(String location) {
-		this.location = location;}
-	
-	public void setFormat(String format) {
-		this.format = format;}
-	
-	public void getInput() {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the book ID = ");
-		bookId = Integer.parseInt(scan.nextLine());
-		System.out.println("Enter the book name = ");
-		bookName = scan.nextLine();
-		System.out.println("Enter the author of the book = ");
-		author = scan.nextLine();
-		//System.out.println("Enter the library name = ");
-		//library = scan.nextLine();
-	}
+    public Book() {
 
-	public abstract void findLocation(String bookName);
-	
-	public String toString() {
-		return "\nBook Id = " + bookId +
-				"\nBook Name = " + bookName +
-				"\nAuthor of the Book = " + author +
-				//"\nLibrary Name = " + library + 
-				"\nLocation of the book in the library = " + location + 
-				"\nFormat of the book = " + format + 
-				"\nBorrewed = " + isBorrowed +
-				"\nDate of borrowed = " + dateBorrowed;}
+    }
+
+    public Book(int bookId, String bookName, String author, String location, String format, String isBorrowed, String dateBorrowed) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.author = author;
+        this.location = location;
+        this.format = format;
+        this.isBorrowed = isBorrowed;
+        this.dateBorrowed = dateBorrowed;
+    }
+
+    // Getters and setters
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getIsBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setIsBorrowed(String isBorrowed) {
+        this.isBorrowed = isBorrowed;
+    }
+
+    public String getDateBorrowed() {
+        return dateBorrowed;
+    }
+
+    public void setDateBorrowed(String dateBorrowed) {
+        this.dateBorrowed = dateBorrowed;
+    }
+
+    public void getInput() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the book ID = ");
+        bookId = Integer.parseInt(scan.nextLine());
+        System.out.print("Enter the book name = ");
+        bookName = scan.nextLine();
+        System.out.print("Enter the author of the book = ");
+        author = scan.nextLine();
+    }
+
+    public abstract void findLocation(String bookName);
+
+    public String toString() {
+        return "\nBook Id = " + bookId +
+                "\nBook Name = " + bookName +
+                "\nAuthor of the Book = " + author +
+                "\nLocation of the book in the library = " + location +
+                "\nFormat of the book = " + format +
+                "\nBorrowed = " + isBorrowed +
+                "\nDate of borrowed = " + dateBorrowed;
+    }
 }
