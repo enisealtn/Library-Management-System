100eecdd66a9fcd4af73ff6d2255fe287df05ee9
NidaAktas <134498190+NidaAktas@users.noreply.github.com>
2023-05-27 13:25:03 +0300

    Enclopedia.java

--- Enclopedia.java
+++ Enclopedia.java
@@ -1,41 +1,52 @@
 import java.util.Scanner;
 
-public class Encyclopedia extends Book{
-	private String subject;
-	private int volume;
-	
-	public Encyclopedia() {
-		
-	}
-	
-	public Encyclopedia(int bookId, String bookName, String author, String location, String format, String isBorrowed, String dateBorrowed, String subject, int volume) {
-		super(bookId, bookName, author, location, format, isBorrowed, dateBorrowed);
-		this.subject = subject;
-		this.volume = volume;
-	}
-	
-	public void getInput() {
-		Scanner scan = new Scanner(System.in);
-		super.getInput();
-		System.out.println("Enter the subject of enclopedia = ");
-		subject = scan.nextLine();
-		System.out.println("Enter the volume of enclopedia = ");
-		volume = Integer.parseInt(scan.nextLine());
-	}
-	
-	@Override
-	public void read() {
-		super.setFormat("Readable");
-	}
+public class Encyclopedia extends Book {
+    private String subject;
+    private int volume;
 
-	@Override
-	public void findLocation(String bookName) {
-		super.setLocation("ENC-"+ bookName.toUpperCase().replace(" ", ""));
-	}
-	
-	public String toString() {
-		return super.toString() + 
-				"\nSubject of the Enclopedia = " + subject +
-				"\nVolume of the Enclopedia = "+ volume;
-	}
+    public Encyclopedia() {
+
+    }
+
+    public Encyclopedia(int bookId, String bookName, String author, String location, String format, String isBorrowed, String dateBorrowed, String subject, int volume) {
+        super(bookId, bookName, author, location, format, isBorrowed, dateBorrowed);
+        this.subject = subject;
+        this.volume = volume;
+    }
+
+    public void getInput() {
+        Scanner scan = new Scanner(System.in);
+        super.getInput();
+        System.out.print("Enter the subject of the encyclopedia: ");
+        subject = scan.nextLine();
+        System.out.print("Enter the volume of the encyclopedia: ");
+        volume = Integer.parseInt(scan.nextLine());
+        System.out.print("Enter the format of the encyclopedia: ");
+        format = scan.nextLine();
+    }
+
+    @Override
+    public void read() {
+        if (format.equalsIgnoreCase("printed")) {
+        	super.setFormat("Readable");
+        } else if (format.equalsIgnoreCase("mp3")) {
+        	super.setFormat("Not Readable");
+        } else {
+        	super.setFormat("Invalid Format");
+        }
+    }
+
+    @Override
+    public void findLocation(String bookName) {
+        super.setLocation("ENC-" + bookName.toUpperCase().replace(" ", ""));
+    }
+
+    @Override
+    public String toString() {
+        String readableStatus = super.getFormat().equals("Readable") ? "Readable" : "Not Readable";
+        return super.toString() +
+                "\nSubject of the Encyclopedia: " + subject +
+                "\nVolume of the Encyclopedia: " + volume +
+                "\nReadability: " + readableStatus;
+    }
 }
